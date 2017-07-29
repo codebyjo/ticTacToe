b7e3ef67a015a5e9fb1501005678d16bfeb70c76
Joanne D <codebyjo@gmail.com>
2017-07-28 21:13:01 -0400

    added click

--- js/app.js
+++ js/app.js
@@ -1,51 +1,109 @@
 
-to win horizontal, match this in any row [true, true, true].
-to win vertical, must match all rows to [true, null, null], or [null, true, null], or [null, null, true]
-to win diagonal, gameArray must match this pattern: [ [true, null, null], [null, true, null], [null, null, true] ] or [ [null, null, true], [null, true, null], [true, null, null] ]
+//to win horizontal, match this in any row [true, true, true].
+//to win vertical, must match all rows to [true, null, null], or [null, true, null], or [null, null, true]
+//to win diagonal, gameArray must match this pattern: [ [true, null, null], [null, true, null], [null, null, true] ] or [ [null, null, true], [null, true, null], [true, null, null] ]
+const PLAYER_TOKEN = "x"
+const COMPUTER_TOKEN = "o";
 
- 
+ $(document).ready(function() {
+      const grid = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
+      ];
+
+  function isGameOver(){
+      //is game over? here check rows
+      for (var i = 0; i <3; i++)
+        if (grid[i][0] !== " " &&
+          grid[i][0] === grid[i][1] &&
+          grid[i][0] === grid[i][2]) {
+          return grid[i][0]
+          }
+        }
+      
+
+      //check column
+   /*   for (var j = 0; j <3; j++)
+        if (grid[0][j] !== " " &&
+          grid[0][j] === grid[1][j] &&
+          grid[0][j] === grid[2][j]) {
+          return grid[0][j]
+          }
+        }
+    
+
+            //check diagonal left top to right down
+        if (grid[0][0] !== " " &&
+          grid[0][0] === grid[1][1] &&
+          grid[0][0] === grid[2][2]) {
+          return grid[0][0]
+          }
+        
+
+      //check diagonal top right to bottom dow
+
+        if (grid[2][2] !== " " &&
+          grid[2][2] === grid[1][1] &&
+          grid[2][2] === grid[0][0]) {
+          return grid[0][2]
+          }
+
+          let empty = true;
+          for (var i=0; i<3; i++) {
+            for (var j = 0; j < 3; j++) {
+              if (grid[i][j]=== " ") {
+                return false;
+              }
+            }
+          }
+          return null;
+        }
+
+      function moveAI() {
+          for (var i=0; i<3; i++) {
+            for (var j = 0; j < 3; j++) {
+             if (grid[i][j]=== " ") {
+                return {
+                  i: i,
+                  j: j
+                };
+              }
+            }
+          }
+          return null;
+        } */
 
 
-//variables
-var gameArray = [ [undefined, undefined, undefined], [undefined, undefined, undefined], [undefined, undefined, undefined] ]; // user = 1, computer = 0
-var userTurn = undefined;
-var currentPlayer = "X";
-var gameIsActive = true; //this tells us when game is active, otherwise game over, prompt start again
+  $('td').click(function(){
+    $this = $(this);
+    $this.html(PLAYER_TOKEN);
+    const i = $this.data('i');
+    const j = $this.data('j');
+      grid[i][j] = PLAYER_TOKEN;
+      console.log(grid);
 
- $(".xButton").click( function() { userTurn = true; });
- $(".oButton").click( function() { userTurn = false; });
+    /*  let gameState = isGameOver()
+      if (gameState) {
+        alert('game over: ' + gameState);
 
- while (gameIsActive) {
+        {if (isGameOver()) {
+      }
 
-      if (userTurn) {
-        userMove();
       } else {
-        computerMove();
+        //move the computer
+        const move = moveAI()
+        grid[move.i][move.j] = COMPUTER_TOKEN;
+      }
+      let gameState = isGameOver()
+      if (gameState) {
+        alert('game over: ' + gameState);
       }
 
-      if (currentPlayer == "X") {
-        currentPlayer = "O";
-      } else {
-        currentPlayer = "X";
-      }
+  */
+  });
 
-      if (winnerFound() == "User" || winnerFound() == "Computer") {
-        userTurn = undefined;
-      }
+ });
 
- }
-
-function winnerFound() {
-  // if winningArray is reached, then stop game, if array is (1,1,1) --> then user wins, if array is (2,2,2) --> then computer wins
-  return "User" or "Computer" or null;
-}
-
-function userMove() {
-  // set listener to all open positions. wait for user to pick, puts position into array, change to 1
-}
-
-function computerMove() {
-  // set randomly to whichever array number is set to 0, change to 2
-}
 
  
