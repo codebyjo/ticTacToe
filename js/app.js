0cd0fb8d69ccbeddd30c61cb20b854e9d5fe1568
Joanne D <codebyjo@gmail.com>
2017-07-28 22:47:46 -0400

    add check for lines

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@
 
 
 
-const PLAYER_TOKEN = "x"
+const PLAYER_TOKEN = "x";
 const COMPUTER_TOKEN = "o";
 
  $(document).ready(function() {
@@ -49,46 +49,39 @@ const COMPUTER_TOKEN = "o";
 
 // when win then game is over,  if winningArray is reached, then stop game,
 
-  function isGameOver(){
+  function isGameOver() {
       //is game over? here check rows
-      for (var i = 0; i <3; i++)
-        if (grid[i][0] !== " " &&
-          grid[i][0] === grid[i][1] &&
-          grid[i][0] === grid[i][2]) {
-          return grid[i][0]
-          }
+      for (var i = 0; i <3; i++) {
+        if ( grid[i][0] !== "" && grid[i][0] === grid[i][1] && grid[i][0] === grid[i][2]) {
+          return grid[i][0];
         }
-      
-// check for winners
+      }
+
+  // check for winners
+
+    //check column
+    for (var j = 0; j <3; j++) {
+      if (grid[0][j] !== " " && grid[0][j] === grid[1][j] && grid[0][j] === grid[2][j]) {
+        return grid[0][j];
+      }
+    }
+
+    //check diagonal left top to right down
+    if (grid[0][0] !== " " && grid[0][0] === grid[1][1] && grid[0][0] === grid[2][2]) {
+      return grid[0][0];
+    }
+
+    //check diagonal top right to bottom dow
+    if (grid[2][2] !== " " && grid[2][2] === grid[1][1] && grid[2][2] === grid[0][0]) {
+      return grid[0][2];
+    }
+  }
 
 
-      //check column
-   /*   for (var j = 0; j <3; j++)
-        if (grid[0][j] !== " " &&
-          grid[0][j] === grid[1][j] &&
-          grid[0][j] === grid[2][j]) {
-          return grid[0][j]
-          }
-        }
+    // alternatve players
+
     
-
-            //check diagonal left top to right down
-        if (grid[0][0] !== " " &&
-          grid[0][0] === grid[1][1] &&
-          grid[0][0] === grid[2][2]) {
-          return grid[0][0]
-          }
-        
-
-      //check diagonal top right to bottom dow
-
-        if (grid[2][2] !== " " &&
-          grid[2][2] === grid[1][1] &&
-          grid[2][2] === grid[0][0]) {
-          return grid[0][2]
-          }
-
-          let empty = true;
+     /*     let empty = true;
           for (var i=0; i<3; i++) {
             for (var j = 0; j < 3; j++) {
               if (grid[i][j]=== " ") {
@@ -99,7 +92,7 @@ const COMPUTER_TOKEN = "o";
           return null;
         }
 
-      function moveAI() {
+   /*   function moveAI() {
           for (var i=0; i<3; i++) {
             for (var j = 0; j < 3; j++) {
              if (grid[i][j]=== " ") {
@@ -113,10 +106,6 @@ const COMPUTER_TOKEN = "o";
           return null;
         } */
 
-/*function userMove(){
-
-};
-*/
 
 
   $('td').click(function(){
@@ -124,8 +113,7 @@ const COMPUTER_TOKEN = "o";
     $this.html(PLAYER_TOKEN);
     const i = $this.data('i');
     const j = $this.data('j');
-      grid[i][j] = PLAYER_TOKEN;
-      console.log(grid);
+    grid[i][j] = PLAYER_TOKEN;
 
 /*function computerMove(){
   }; */
@@ -151,6 +139,5 @@ const COMPUTER_TOKEN = "o";
   });
 
  });
-
 
  
