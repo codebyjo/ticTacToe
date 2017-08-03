e5d3845d99a05c8da27df15c0f47a2215782cc5e
eagarcia8 <eagarcia8@gmail.com>
2017-08-02 21:08:02 -0700

    Fixed visual bugs, added modal for selecting player token

--- js/app.js
+++ js/app.js
@@ -1,121 +1,134 @@
+/* while (gameIsActive) {
 
-//to win horizontal, match this in any row [true, true, true].
-//to win vertical, must match all rows to [true, null, null], or [null, true, null], or [null, null, true]
-//to win diagonal, gameArray must match this pattern: [ [true, null, null], [null, true, null], [null, null, true] ] or [ [null, null, true], [null, true, null], [true, null, null] ]
+     if (userTurn) {
+       userMove();
+     } else {
+       computerMove();
+     }
 
+     if (currentPlayer == "X") {
+       currentPlayer = "O";
+     } else {
+       currentPlayer = "X";
+     }
 
-//var gameArray = [ [undefined, undefined, undefined], [undefined, undefined, undefined], [undefined, undefined, undefined] ]; // user = 1, computer = 0
-//var userTurn = undefined;
-//var currentPlayer = "X";
-//var gameIsActive = true; //this tells us when game is active, otherwise game over, prompt start again
+     if (winnerFound() == "User" || winnerFound() == "Computer") {
+       userTurn = undefined;
+     }
 
-//choosing to be x or o
+}  */
 
- $(".xButton").click( function() { userTurn = true; });
- $(".oButton").click( function() { userTurn = false; });
+let playerToken;
+let computerToken;
 
- /* while (gameIsActive) {
+$(document).ready(function() {
 
-      if (userTurn) {
-        userMove();
-      } else {
-        computerMove();
-      }
+ $("#xButton").click( () => {
+   userTurn = true;
+   playerToken = "X";
+   computerToken = "O";
+   $("#modal-background").hide();
+   placeListeners();
+ });
 
-      if (currentPlayer == "X") {
-        currentPlayer = "O";
-      } else {
-        currentPlayer = "X";
-      }
+ $("#oButton").click( () => {
+   userTurn = false;
+   playerToken = "O";
+   computerToken = "X";
+   $("#modal-background").hide();
+   placeListeners();
+ });
 
-      if (winnerFound() == "User" || winnerFound() == "Computer") {
-        userTurn = undefined;
-      }
-
- }  */
-
-
-
-
-const PLAYER_TOKEN = "x";
-const COMPUTER_TOKEN = "o";
-
- $(document).ready(function() {
-      const grid = [
-      ['', '', ''],
-      ['', '', ''],
-      ['', '', '']
-      ];
+ const grid = [
+   ['', '', ''],
+   ['', '', ''],
+   ['', '', '']
+ ];
 
 // when win then game is over,  if winningArray is reached, then stop game,
 
-  function isGameOver() {
-      //is game over? here check rows
-      for (var i = 0; i <3; i++) {
-        if ( grid[i][0] !== "" && grid[i][0] === grid[i][1] && grid[i][0] === grid[i][2]) {
-          return grid[i][0];
-        }
-      }
+ function isGameOver() {
+     //is game over? here check rows
+     for (var i = 0; i <3; i++) {
+       if ( grid[i][0] !== "" && grid[i][0] === grid[i][1] && grid[i][0] === grid[i][2]) {
+         return grid[i][0];
+       }
+     }
 
-  // check for winners
+ // check for winners
 
-    //check column
-    for (var j = 0; j <3; j++) {
-      if (grid[0][j] !== " " && grid[0][j] === grid[1][j] && grid[0][j] === grid[2][j]) {
-        return grid[0][j];
-      }
-    }
+   //check column
+   for (var j = 0; j <3; j++) {
+     if (grid[0][j] !== " " && grid[0][j] === grid[1][j] && grid[0][j] === grid[2][j]) {
+       return grid[0][j];
+     }
+   }
 
-    //check diagonal left top to right down
-    if (grid[0][0] !== " " && grid[0][0] === grid[1][1] && grid[0][0] === grid[2][2]) {
-      return grid[0][0];
-    }
+   //check diagonal left top to right down
+   if (grid[0][0] !== " " && grid[0][0] === grid[1][1] && grid[0][0] === grid[2][2]) {
+     return grid[0][0];
+   }
 
-    //check diagonal top right to bottom dow
-    if (grid[2][2] !== " " && grid[2][2] === grid[1][1] && grid[2][2] === grid[0][0]) {
-      return grid[0][2];
-    }
-  }
+   //check diagonal top right to bottom dow
+   if (grid[2][2] !== " " && grid[2][2] === grid[1][1] && grid[2][2] === grid[0][0]) {
+     return grid[0][2];
+   }
+ }
 
-
-    // alternatve players
-
-    
-     /*     let empty = true;
-          for (var i=0; i<3; i++) {
-            for (var j = 0; j < 3; j++) {
-              if (grid[i][j]=== " ") {
-                return false;
-              }
-            }
-          }
-          return null;
-        }
-
-   /*   function moveAI() {
-          for (var i=0; i<3; i++) {
-            for (var j = 0; j < 3; j++) {
-             if (grid[i][j]=== " ") {
-                return {
-                  i: i,
-                  j: j
-                };
-              }
-            }
-          }
-          return null;
-        } */
-
-
+ function placeListeners() {
 
   $('td').click(function(){
-    $this = $(this);
-    $this.html(PLAYER_TOKEN);
+    let $this = $(this);
+    $this.html(playerToken);
     const i = $this.data('i');
     const j = $this.data('j');
-    grid[i][j] = PLAYER_TOKEN;
+    grid[i][j] = playerToken;
+  });
 
-/*function computerMove(){
+  $('td').hover( function() {
+    let $this = $(this);
+
+    if ($this.text() === '\xa0') {
+      $this.css('background-color', '#bdc3c7');
+      $this.css('cursor', 'pointer');
+    }
+  }, function() {
+    let $this = $(this);
+    $this.css('background-color', 'transparent');
+    $this.css('cursor', 'default');
+  });
+
+ }
+
+   // alternatve players
+
+
+    /*     let empty = true;
+         for (var i=0; i<3; i++) {
+           for (var j = 0; j < 3; j++) {
+             if (grid[i][j]=== " ") {
+               return false;
+             }
+           }
+         }
+         return null;
+       }
+
+  /*   function moveAI() {
+         for (var i=0; i<3; i++) {
+           for (var j = 0; j < 3; j++) {
+            if (grid[i][j]=== " ") {
+               return {
+                 i: i,
+                 j: j
+               };
+             }
+           }
+         }
+         return null;
+       } */
+
+ /*function computerMove(){
   }; */
 
     /*  let gameState = isGameOver()
@@ -136,8 +149,5 @@ const COMPUTER_TOKEN = "o";
       }
 
   */
-  });
 
- });
-
- 
+});
